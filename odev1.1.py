930711ebcae09045071972dd515453cb28869834
hilalkrl <hilal.krl97@gmail.com>
2022-11-17 11:28:32 +0300

    first commit

---
+++ odev1.1.py
@@ -0,0 +1,11 @@
+sinav_sonucu1 = 75
+sinav_sonucu2 = 80
+
+if sinav_sonucu1>sinav_sonucu2:
+    print("Sınav sonucunda düşüş var.")
+
+if sinav_sonucu1<sinav_sonucu2:
+    print("Sınav sonucunda yükseliş var.")
+
+if sinav_sonucu1==sinav_sonucu2:
+    print("Sınav sonucunda değişiklik yok.")
---
+++ odev1.py
@@ -0,0 +1,52 @@
+print("Merhaba Etiya")
+#! string = metinsel ifade 
+text = "10"
+print(text)
+
+#! integer = tam sayı
+number = 5 
+print(number)
+
+#! double,float,decimal = ondalıklı sayı 
+fnumber = 5.3 
+print(fnumber)
+
+#! matematiksel operatörler
+print(number + 5)
+print(number - 5)
+print(number * 5)
+print(number / 5)
+print(number % 3) #bölümünden kalanı verir
+
+#! boolean,bool = true veya false
+isVerified = True
+
+
+#! mantıksal(koşul) operatörler = her msntıksal operatör boolean deper döner
+print(1 == 2) #eşit midir diye sorgular
+print(1 !=2 )  #eşit olmamasını ister
+print( 1 > 2 )
+print(1 < 2)
+
+print(10%2 == 0)
+
+
+#! string operasyonları 
+text = "Merhaba Etiya"
+print(text.upper())
+print(text.lower())
+print(text.startswith("Mer"))
+print(text.endswith("Etiya"))
+
+
+name = "Hilal" 
+age = "25"
+company = "Etiya" 
+
+#Hilal 25 yaşında Etiya'da çalışıyor
+print(name + " " + age + " yaşında " + company + "'da çalışıyor")
+print(f"{name} {age} yaşında {company} 'da çalışıyor")
+
+
+
+
